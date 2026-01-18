7398f213ecd2d36ceeb0653497c144eb204e9e3c
hope-corp <kelvinoduro544@gmail.com>
2026-01-18 19:29:03 +0000

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,1 +1,22 @@
 
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+import uvicorn
+
+app = FastAPI()
+
+# Mount the 'static' folder for CSS and Images
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# Point to the 'templates' folder for HTML
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    # This sends index.html to the user's browser
+    return templates.TemplateResponse("index.html", {"request": request})
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
